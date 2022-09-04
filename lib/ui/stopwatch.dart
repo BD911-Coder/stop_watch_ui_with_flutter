cddf21028cf94b99acf5e6115ea11359656564a7
BD911-Coder <68704206+BD911-Coder@users.noreply.github.com>
2022-09-05 01:15:50 +0300

    Initial Commit

---
+++ lib/ui/stopwatch.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'elapsed_time_text_basic.dart';
+
+class Stopwatch extends StatefulWidget {
+  @override
+  _StopwatchState createState() => _StopwatchState();
+}
+
+class _StopwatchState extends State<Stopwatch> {
+  @override
+  Widget build(BuildContext context) {
+    return ElapsedTimeTextBasic(
+      elapsed: Duration(seconds: 5),
+    );
+  }
+}
