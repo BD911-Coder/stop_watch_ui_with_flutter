03f968ef7ac2912bb980c9e52fe7c5097688d825
BD911-Coder <68704206+BD911-Coder@users.noreply.github.com>
2022-09-05 18:03:33 +0300

    ClockMarker.dart added

--- lib/screens/stopwatch_renderer.dart
+++ lib/screens/stopwatch_renderer.dart
@@ -17,15 +17,6 @@ class StopwatchRenderer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              width: 3,
-              color: Colors.orange,
-            ),
-            borderRadius: BorderRadius.circular(radius),
-          ),
-        ),
         Positioned(
           left: radius,
           top: radius,
