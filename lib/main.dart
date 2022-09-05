2b8801f364b73cac7f75274f990e19c2b72ae18c
BD911-Coder <68704206+BD911-Coder@users.noreply.github.com>
2022-09-05 04:01:02 +0300

    Stopwatch Renderer Added

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
--- lib/screens/stopwatch.dart
+++ lib/screens/stopwatch.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:stop_watch/screens/elapsed_time_text.dart';
 
 import 'elapsed_time_text_basic.dart';
 
@@ -9,28 +11,33 @@ class Stopwatch extends StatefulWidget {
   _StopwatchState createState() => _StopwatchState();
 }
 
-class _StopwatchState extends State<Stopwatch> {
-  late DateTime _initialTime;
+class _StopwatchState extends State<Stopwatch>
+    with SingleTickerProviderStateMixin {
   Duration _elapsedTime = Duration.zero;
+  late final Ticker _ticker;
 
   @override
   void initState() {
     super.initState();
-    _initialTime = DateTime.now();
-    Timer.periodic(
-      Duration(microseconds: 50),
-      (_timer) {
-        final now = DateTime.now();
+    _ticker = createTicker(
+      (elapsed) {
         setState(() {
-          _elapsedTime = now.difference(_initialTime);
+          _elapsedTime = elapsed;
         });
       },
     );
+    _ticker.start();
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ElapsedTimeTextBasic(
+    return ElapsedTimeText(
       elapsed: _elapsedTime,
     );
   }
