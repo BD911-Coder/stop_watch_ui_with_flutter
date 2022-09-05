c71cd0bb999f0df908a8272bc3155ac5877328c7
BD911-Coder <68704206+BD911-Coder@users.noreply.github.com>
2022-09-05 20:48:36 +0300

    Stopwatch App with Flutter Completed!

--- lib/screens/stopwatch.dart
+++ lib/screens/stopwatch.dart
@@ -13,8 +13,12 @@ class Stopwatch extends StatefulWidget {
 
 class _StopwatchState extends State<Stopwatch>
     with SingleTickerProviderStateMixin {
-  Duration _elapsedTime = Duration.zero;
+  Duration _previouslyElapsedTime = Duration.zero;
+  Duration _currentlyElapsedTime = Duration.zero;
+  Duration get _elapsedTime => _previouslyElapsedTime + _currentlyElapsedTime;
+
   late final Ticker _ticker;
+  bool _isRunning = false;
 
   @override
   void initState() {
@@ -22,17 +26,38 @@ class _StopwatchState extends State<Stopwatch>
     _ticker = createTicker(
       (elapsed) {
         setState(() {
-          _elapsedTime = elapsed;
+          _currentlyElapsedTime = elapsed;
         });
       },
     );
-    _ticker.start();
   }
 
   @override
   void dispose() {
     _ticker.dispose();
     super.dispose();
+  }
+
+  void _toggleRunning() {
+    setState(() {
+      _isRunning = !_isRunning;
+      if (_isRunning) {
+        _ticker.start();
+      } else {
+        _ticker.stop();
+        _previouslyElapsedTime += _currentlyElapsedTime;
+        _currentlyElapsedTime = Duration.zero;
+      }
+    });
+  }
+
+  void _reset() {
+    _ticker.stop();
+    setState(() {
+      _isRunning = false;
+      _previouslyElapsedTime = Duration.zero;
+      _currentlyElapsedTime = Duration.zero;
+    });
   }
 
   @override
@@ -52,7 +77,7 @@ class _StopwatchState extends State<Stopwatch>
                 width: 80,
                 height: 80,
                 child: ResetButton(
-                  onPressed: () {},
+                  onPressed: _reset,
                 ),
               ),
             ),
@@ -62,8 +87,8 @@ class _StopwatchState extends State<Stopwatch>
                 width: 80,
                 height: 80,
                 child: StartStopButton(
-                  isRunning: true,
-                  onPressed: () {},
+                  isRunning: _isRunning,
+                  onPressed: _toggleRunning,
                 ),
               ),
             )
