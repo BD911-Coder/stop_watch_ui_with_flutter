2a13c1fa52d2a9c8aee33f0b286449b464decf78
BD911-Coder <68704206+BD911-Coder@users.noreply.github.com>
2022-09-05 17:40:01 +0300

    ClockHand.dart added

---
+++ lib/screens/widgets/clock_hand.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ClockHand extends StatelessWidget {
+  const ClockHand(
+      {super.key,
+      required this.rotationZAngle,
+      required this.handThickness,
+      required this.handLength,
+      required this.color});
+
+  final double rotationZAngle;
+  final double handThickness;
+  final double handLength;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
