9a3285e413bb2a12fa07962568d6a3aa0a00258d
BD911-Coder <68704206+BD911-Coder@users.noreply.github.com>
2022-09-05 18:06:17 +0300

    ClockMarker class updated

--- lib/screens/widgets/clock_markers.dart
+++ lib/screens/widgets/clock_markers.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ClockMarker extends StatelessWidget {
-  const ClockMarker({Key? key}) : super(key: key);
+class ClockSecondsTickMarker extends StatelessWidget {
+  const ClockSecondsTickMarker({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
